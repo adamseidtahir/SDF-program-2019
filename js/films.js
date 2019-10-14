51be9ecf5a7c9499edf5b8f8f08c5ad12c7d4488
Adam Seid Tahir <adam.seidtahir@hotmail.com>
2019-10-15 00:03:54 +0200

    Added modals with film info

---
+++ js/films.js
@@ -0,0 +1,148 @@
+//Add only the firstword as name for film
+
+var info = {
+    "films": {
+        "Clusters": {
+            "title": "Clusters I & II",
+            "description": "Clusters I ( a solo for dancer Rachel Tess ) and II ( a duet for dancers Agnieszka Dlugoszewska and Anna Pehrsson ) organise bodies into movement objects-circuits of energies, vibrations and flows where the gap between the problem and its solution becomes the dance. Slowly gliding through, in-between and outside of themselves and each other, these bodies are partly in symbiosis and partly out of reach. The work meets the audience as an ongoing meditation on proximity and intimacy, an invitation to other ways of being together."
+        },
+        "Square": {
+            "title": "Square of the Star",
+            "description": "A hybrid documentary exploring how choreography can be used as a tool in filmmaking. The filmmaker Karl-Oskar Gustafsson explores Karlaplan in Stockholm – a square built in the shape of a star. He creates a pattern where people walk in different orbits like planets, circling around and into the fountain, making encounters with each other. Interviews became the dialogue in the film."
+        },
+        "SKINNING/TRUE": {
+            "title": "SKINNING/TRUE MESH/16:9 full time texture",
+            "description": "Creating surreal analog 3D animation by combining choreography, costume design and film."
+        },
+        "Mayfly": {
+            "title": "Mayfly",
+            "description": "A celebration of the interchangeability of everything. The short and spontaneous improvisation took place at the Circus Exhibition PAPERSCAPES by Eduardo Cardozo Hidalgo (PER) and was filmed with a cellphone."
+        },
+        "Linn": {
+            "title": "Linn",
+            "description": "LINN is a solo about hope, effort, persistence and achievement."
+        },
+        "The": {
+            "title": "The time it takes",
+            "description": "to arrive\nto live \nto make a land \nto find \nto fall \nto settle in \nto work \nto build \nto seize the day \nto journey\nto be happy \nto make a story \nto dance \n \nto pick up the pieces \nto laugh \nto sing a song \nto love \nto grow \nto bury the dead \nto lose \nto uncover the past \nto walk \nto imagine the future \nto leave \nto return \n \nthe time, \n \nit takes"
+        },
+        "Traces": {
+            "title": "Traces",
+            "description": "A man and a woman together alone in a relationship that seems to drain them both. Is this the end or the beginning of the end? The film Traces takes its inspiration from Jason Mabana’s dance piece ‘Last Line’ which he created for the JMD/Urban Contemporary Department of the Amsterdam School of the Arts in 2018. Director Nicola Hepp felt a deep connection to his work and juxtaposed the movement material with ideas of representing a failing relationship. The underlying sense of time, rush and fragility informs the viewer of a sadness inherent to the duet."
+        },
+        "Blind": {
+            "title": "Blind Dancer",
+            "description": "Lisa is a blind librarian from Oslo who can't stop dancing. She accepts the challenge, to step out of her comfort zone, leave her guide dog behind and travel to Molenbeek in Brussels, to dance with Said Gharbi, a blind, top trained, professional Moroccan dancer."
+        },
+        "ON": {
+            "title": "ON & ON",
+            "description": "Avatar of Akihiko Taniguchi appears in the virtual space. This avatar suddenly starts dancing to the music. However, artist Akihiko himself has never danced. It dances based on data downloaded from an online motion library. The movement of someone in the past has made this avatar dance. And this avatar can be replicated and has no real body. It is therefore possible to dance in the same place, penetrating each other. This work includes new ontology and body theory in virtual space. There are various things gathered together due to their history. And it is a repetitive world that mimics our real world as an entity without physical mass or body."
+        },
+        "Interstice": {
+            "title": "Interstice",
+            "description": "Probing the illusive boundary itself, Interstice presents the veil as a self-contained magic trick: a shapeshifting second skin loaded with potential energy to manipulate identities and temporal-spatial dimensions that would otherwise be rendered immeasurable in its absence.  Interstice is an attempt to pass through the veil that obscures our collective vision to reach a space in-between.  An interstitial space."
+        },
+        "Good": {
+            "title": "Good Tidings",
+            "description": "When the climate in the Mediterranean returns mild, boats full of migrants leave from the coasts of Africa and Asia trying to reach Europe, the 'promised land'. On a summer night, Bochra arrived on a beach in Sicily with over 130 people on board. All soon escaped. And Bochra remained alone in the limbo between sea and earth… until the tourists arrived."
+        },
+        "Things": {
+            "title": "Things that start slowly",
+            "description": "This film triptych features images of a young baby and two women. The women are seen both in early pregnancy and then later at about nine months. Running through it is a pervading sense of loss; a restless tension between things that remain, and things that disappear over time."
+        },
+        "∞": {
+            "title": "∞",
+            "description": "Drawing inspiration from Bach’s Goldberg variations, ∞ reflects upon the purity of existence, and the cyclical, yet contradictory way in which life and death lead to, and become one another."
+        },
+        "33": {
+            "title": "33 questions",
+            "description": "'All i know is that i know nothing' - This 2400 years old statement of Socrates applies perfectly to the idea behind my movie. No matter how much we search, think, wonder and eplore, some questions are left unanswered. Because behind each answer lies another question."
+        },
+        "Upp": {
+            "title": "Upp till dans",
+            "description": "The year 2014 Sara Lindström visited the retirement home Parkvägen in Österbybruk to create a film together with some of their residents. Dance and movement became openings to stories and dialogues through a film about both the small and big things in life."
+        },
+        "Blinktråd": {
+            "title": "Blinktråd",
+            "description": "Dance film from some moments with blinking threads."
+        },
+        "Cygnus": {
+            "title": "Cygnus",
+            "description": "A short screendance filmed on location on Battle Lake, MN. Cygnus invites us to revel in the beauty of the sunrise over calm waters as the moon slips behind the horizon. A celebration of the earth, the body, and their kinship."
+        },
+        "Sisters": {
+            "title": "Sisters",
+            "description": "Three sisters grow up in a broken home. They are at each other’s mercy and survive as long as they are together. But is their loving connection strong enough to endure their gloomy surroundings?"
+        },
+        "He": {
+            "title": "He loves my eyes",
+            "description": "Tican, the story between the man and the wicked. A documentary about love, dance and music, freedom and prison."
+        },
+        "Blånagla": {
+            "title": "Blånagla",
+            "description": "Movements and memories, fused, from a life spent working in a papermill factory, distilled and embodied through music and dance."
+        },
+        "Dancing": {
+            "title": "Dancing in Silk",
+            "description": "Since the early 1990s, the art of Khmer classical ballet has found a new life in the ethnic neighborhood of Cambodia Town in Long Beach, California with the passion of Sophiline Cheam Shapiro, master dancer and survivor of the Khmer Rouge genocide. Today, it is up to Khannia Ok, her protégé, to pass on this knowledge to a new generation of Cambodian-Americans, especially young girls, who grew up far from their roots. With the creation of a new show, students embark on a transformative experience into their Cambodian identity through movement, costumes and community."
+        },
+        "Platform": {
+            "title": "Platform 13",
+            "description": "Platform 13 tells the tragicomic story of a Japanese railway conductor. With his equally strong as graceful movements he keeps the biggest crowds under control. But than he ends up in his biggest nightmare: a strange new world, in which he and his signals prove unnecessary."
+        },
+        "I": {
+            "title": "I care",
+            "description": "A woman moves throught moments, in subtle ways responding and observing layers in herself and her surroundings. Does her way of sensing belong? a film poem to enjoy and reflect upon."
+        },
+        "Strange": {
+            "title": "Strange birds",
+            "description": "There are lots of things that we cannot explain- and yet they happen… Strange Birds is a danced roadmovie with two lonely men that happen to meet in a frosty landscape. How did they end up there and do they dare to meet within a tango? About things that happen without any reason but grows to something greater."
+        },
+        "Iceolo": {
+            "title": "Iceolo",
+            "description": "A story told through movement and cinematography with the aim of highlighting the beauty of emotionality rather than point on the issues that divides and fractures us today. The merge of an inner emotional battle and a troublesome meeting between the elements Ice, Heat and Water. Due to toxicity in home-based area. The three are forced to an unknown field. A place of risk. A place where nothing is safe or comfortable. The following element does not feel relatable at all too them although the mission was to save them from their misery."
+        },
+        "Anasa": {
+            "title": "Anasa",
+            "description": "The right to breath and to feel the fall. Earthquake. Inside or outside. A connection through matter and its dissolution. The earth, the stone, the sand, the sea. Bodies. Connected. Dry flowers pushing out from the sand. Dissolving. Filled bodies, unstoppable. Earth bodies, floating inside or outside. The right to exist, to simply be."
+        },
+        "Mass": {
+            "title": "Mass",
+            "description": "MASS is a 10 minutes single take video-dance shot in Paris. The project was framed within the Danse en Seine's choreographic workshops, including 40 amateur dancers. Images of the crowd appear more and more often and symbolize the current upheavals all over the world, evoking alternately parties, migrations, manifestations, gatherings or just the daily life of big cities. We thus work on the mass, with all the drunkenness it can inspire. We confront the individual with crowd movements, in order to observe how he resists or lets immerse himself."
+        },
+        "On": {
+            "title": "On animals and cleaning",
+            "description": "Sweden, like most countries, is built on fascism. In the first half of the 20th century, Sweden was world-leader in eugenics, and provided the German Nazis with 'research' during the 2nd World War. The Swedish state sterilized tens of thousands of people within their obsession with cleaning and hygiene. Today, the fascist obsession with cleaning lives on. There are absurd designs in Sweden that try to fool us to think that 'less is more', that there is such a thing as 'blond wood', or that design, or even a line, can be 'clean'. On Animals and Cleaning brings up Sweden's dirty secrets through the absurd performative act of cleaning. The film is based on a performance with the same name."
+        },
+        "Off": {
+            "title": "Off",
+            "description": "Derived from the courageous stories of migration in boats and contradicted with the derogatory term 'FOB', Off is an exploration of bodies in misplacement. This film makes palpable a vulnerable experience through the sensibilities and motions seen on the skin, spine, hands, and feet. The camera creates a sense of ungrounding, situation the body in a void. Off aims to create an emotional landscape through physicalizing the cinematic experience in order to seize a deeper human connection."
+        },
+        "Shapeshifters": {
+            "title": "Shapeshifters",
+            "description": "Part Three of the Sex & Space Trilogy. After two porn films with dancers, this is a dance film with porn stars, where Jiz Lee, Gala Vanting, KAy Garnellen, Bishop Black, & Jasko Fide talk about their performance practice & try out some contemporary dance improv tasks. No sex or even nudity. Radical!"
+        },
+        "Framed": {
+            "title": "Framed",
+            "description": "A film made intuitively from improvised material. During the time I made this film, I was dealing with a lot of stress and trying to ignore the voices in my head."
+        },
+        "Jah": {
+            "title": "Jah Intervention",
+            "description": "Considering the research of the Brazilian Forum of Public Security (2017), Black people in Brazil are still more than half of the population of the country. Between 2005 and 2015 the number of black people murdered increased by 18% and this also made us the majority of homicide victims, accounting for 71% of all registered bodies. 'Jah Intervention' is a symbolic walk to exhaustion. The intervention proposes the preliminary warm-up that precedes a fight of titans in a boxing ring. The intervention consists of the movement of the performer sensing the sudden fall when affected by perforations by bullets of semi-automatic weapons."
+        },
+        "Isotropic": {
+            "title": "Isotropic Dreams",
+            "description": "ISOTROPIC DREAMS is a choreographic edit of the ballet studio environment where a subject is diffracted, yet contained. Alone and with others, the subject looks for meaning in a superficial world that furnishes strange desires. With high hopes, the subject's work is constituted by opposing wishes: to be rid of the conditions of the studio, and to never leave it."
+        },
+        "LeDomain": {
+            "title": "LeDomain",
+            "description": "LeDomain is a music video for a Hungarian alternative rock band's song 'ledomain'. This grotesque project was inspired by the reigning kings of Hungarian public transportation: the ticket controllers. With it's surrealistic approach the video clip tries to articulate the feelings of an everyday traveller as well as the controllers - who are all seem to be victims of the system."
+        }
+    },
+    "artists": {
+        "Pehrsson": {
+            "name": "Anna Pehrsson",
+            "about": "Hej, jag heter Anna"
+        }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,115 @@
+
+//Refresh on resize, so evrything scales correctly
+window.onresize = function(){ location.reload(); }
+
+//Top section animations
+TweenMax.from('#line', 1, {marginLeft:"-100%"});
+TweenMax.from('#sdfLogo', 1, {marginTop:"-100%", delay: 0.5});
+TweenMax.from('#locBox', 1, {left:"100vw", delay: 1.5});
+TweenMax.from('#ticket', 1, {left:"100vw", delay: 1.5});
+TweenMax.from('nav', 1, {left: "-60vh", delay: 1.5, ease: Power2.easeOut});
+
+
+// Adds listener to all titles, they need to be turned into 
+// anchor tags in HTML file for it to work.
+let elementsArray = document.querySelectorAll(".filmTit"); 
+//Add listener that triggers a modal with word-info
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+        infoWindow(this.text)}, true);
+        console.log(elem.text);
+});
+
+
+//Open a modal window with info about the word that is clicked
+function infoWindow(word){
+
+    //Call to findInfo function that sends a request for info about selected word
+    findInfo(word);
+
+    //Boolean keeping track of whether modal is open or not
+    let modalOpen = true;
+
+    // Get the span element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the span (the X), close the modal
+    span.onclick = function() {
+        
+        modalCloseAnim();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //     if (event.target != modal && modalOpen == true) {
+    //         modalCloseAnim();
+    //         modalOpen = false;
+    //     }
+    // }
+
+}
+
+
+//Function that retrieves information about selected word and outputs it as HTML in a list
+function findInfo(word){
+
+    //Prepare to take only the first word of the sent word
+    var name = word.split(' ');
+
+    console.log(name);
+
+
+    //Get the modText container that will hold the text
+    const modText = document.getElementById("modText");
+
+    //Empty element from potential previous text
+    modText.innerHTML = "";
+
+
+        //Make the modal visible
+        modalOpenAnim();
+
+            //Transform JSON string to object
+            var film = info.films[name[0]];
+
+            //If there's NO word-info...
+            if (film.resultCode == "NO_DATA"){
+
+                errorMessage("Didn't find any information about this word...");
+
+            //If there IS word-info...
+            } else {
+
+                
+                //Set chosen word as header
+                const h1 = document.createElement("h1");
+                h1.textContent = film.title;
+                modText.appendChild(h1);
+
+                //Create list and list items
+                const list = document.createElement("ul");
+                const description = document.createElement("li");
+
+                //Add content to the listitems
+                description.textContent = film.description;
+
+                //Append all elements to the modText container
+                modText.appendChild(list);
+                list.appendChild(description);
+            }
+}
+
+
+
+
+//ANIMATIONS
+
+function modalOpenAnim(){
+    TweenMax.to('#modal', 0.5, {display: "block", opacity: 1, delay: 0, ease: Power2.easeInOut});
+    TweenMax.to('#shadow', 0.5, {display: "block", opacity: 1, delay: 0, ease: Power2.easeInOut});
+}
+
+function modalCloseAnim(){
+    TweenMax.to('#modal', 0.5, {display: "none", opacity: 0, delay: 0, ease: Power2.easeInOut});
+    TweenMax.to('#shadow', 0.5, {display: "none", opacity: 0, delay: 0, ease: Power2.easeInOut});
+}
